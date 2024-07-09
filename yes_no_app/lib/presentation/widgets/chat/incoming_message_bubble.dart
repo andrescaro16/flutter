7becd44afc52fdad338319bd95f6b197ad1d59bf
andrescaro16 <julicaro2003@gmail.com>
2024-07-09 11:02:10 -0500

    Añadir indicadores de carga para la imagen en la burbuja de mensajes

--- yes_no_app/lib/presentation/widgets/chat/incoming_message_bubble.dart
+++ yes_no_app/lib/presentation/widgets/chat/incoming_message_bubble.dart
@@ -49,9 +49,44 @@ class _ImageBubble extends StatelessWidget {
       borderRadius: BorderRadius.circular(20),
       child: Image.network(
         'https://yesno.wtf/assets/yes/5-64c2804cc48057b94fd0b3eaf323d92c.gif',
-        fit: BoxFit.cover,
         width: size.width * 0.8,
         height: 150,
+        fit: BoxFit.cover,
+        loadingBuilder: (context, child, loadingProgress) {
+          // Forma 1
+          if (loadingProgress == null) {
+            return child;
+          }
+
+          return Container(
+            width: size.width * 0.8,
+            height: 150,
+            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+            child: const Center(child: Text('The Goat is sending a gif...')),
+          );
+
+          // Forma 2
+          // if (loadingProgress == null) {
+          //   return child;
+          // }
+
+          // return const Center(
+          //   child: CircularProgressIndicator(),
+          // );
+
+          // Forma 3
+          // if (loadingProgress == null) {
+          //   return child;
+          // }
+          // return Center(
+          //   child: CircularProgressIndicator(
+          //     value: loadingProgress.expectedTotalBytes != null
+          //         ? loadingProgress.cumulativeBytesLoaded /
+          //             loadingProgress.expectedTotalBytes!
+          //         : null,
+          //   ),
+          // );
+        },
       ),
     );
   }
